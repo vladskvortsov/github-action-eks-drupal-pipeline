a6661ffccfcd34999850b2c12689ec75d590f8ff
vlad <vladskvortsov96@gmail.com>
2023-06-19 15:29:42 +0300

    action added

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -37,4 +37,20 @@ cidr_blocks = [
             "10.0.0.0/8"
         ]
     }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+            "10.0.0.0/8"
+        ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+            "10.0.0.0/8"
+        ]
+  }
 }
