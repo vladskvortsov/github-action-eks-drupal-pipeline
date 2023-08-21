196c595517b30ff41726de71df2fdbb0d46556fb
vlad <vladskvortsov96@gmail.com>
2023-08-21 20:45:05 +0300

    action fixed

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
      source = "hashicorp/aws"
-     version = "~> 5.3"
+     version = "=> 5.0"
     }
 #     ansible = {
 #      source  = "ansible/ansible"      
