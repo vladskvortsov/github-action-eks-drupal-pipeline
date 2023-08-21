e654f949d760dcc2d0e6255e89a0fd790f8b6cee
vlad <vladskvortsov96@gmail.com>
2023-08-21 20:38:22 +0300

    action fixed

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,11 @@
-provider "aws" {
-    region = "${var.region}"
-#    access_key = "${var.access_key}"
-#    secret_key = "${var.secret_key}"
-}
+
 
 
 terraform {
   required_providers {
     aws = {
      source = "hashicorp/aws"
-#     version = "~> 5.3"
+     version = "~> 5.3"
     }
 #     ansible = {
 #      source  = "ansible/ansible"      
@@ -23,4 +19,9 @@ terraform {
     key = "terraform.tfstate"
   }
 
+}
+provider "aws" {
+    region = "${var.region}"
+#    access_key = "${var.access_key}"
+#    secret_key = "${var.secret_key}"
 }
