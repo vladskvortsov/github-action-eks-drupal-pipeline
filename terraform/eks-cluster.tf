36d0e9acce573a0a486b74d12f286f758a1eeec8
vladskvortsov <vladskvortsov96@gmail.com>
2023-08-31 22:31:03 +0300

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -33,12 +33,3 @@ module "eks" {
 }
 
 
-module "efs_csi_driver" {
-  source = "git::https://github.com/DNXLabs/terraform-aws-eks-efs-csi-driver.git"
-
-  cluster_name                     = module.eks.cluster_id
-  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
-}
-
-
